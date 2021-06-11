43728ee10f988a8fdbb7534f6bbc1e07af7a76a6
duixin111 <74460899+duixin111@users.noreply.github.com>
2021-06-11 21:02:36 +0800

    八大排序
    插入排序、希尔排序、选择排序、快速排序、冒泡排序、计数排序、堆排序、归并排序

---
+++ My_Sort/MySort.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+void InsertSort(int array[], int size);
+void ShellSort(int array[], int size);
+
+void SelectSort(int array[], int size);
+
+void QuickSort(int array[], int left, int right);
+void QuickSortNor(int array[], int size);
+
+void CountSort(int array[], int size);
+
+void HeapSort(int array[], int size);
+
+void BubbleSort(int array[], int size);
+
+//πÈ≤¢≈≈–Ú
+void MergeSort(int array[], int size);
+
+
+void PrintArray(int array[], int size);
+
+//void Swap(int* a, int* b);
